dacb3f1b96d4f32b6d52861782ee60d6666a0cd0
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-12 07:36:44 -0400

    Display comment when form is submitted

--- src/TextField.js
+++ src/TextField.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 
 class TextField extends Component {
+    constructor() {
+        super()
+        this.state = {
+            text: '',
+            comments: []
+            
+        }
+    }
+
+    setText(ev) {
+        this.setState({text: ev.target.value})
+    }
+    
+    displayComment(ev) {
+        if(this.state.text != '') {
+            const comments = [...this.state.comments]
+            comments.push(this.state.text)
+            this.setState({comments})
+            this.state.text = ''
+        }
+        ev.target.parentElement.reset()
+    }
     render() {
         return (
-            <form id = {this.props.display}>
-                <input type = 'text'/>
-            </form>
+            <div>
+                <form id = {this.props.display}>
+                    <input type = 'text' onChange = {(ev) => this.setText(ev)} />
+                    <button type = 'submit' onClick = {(ev) => this.displayComment(ev)}>comment</button>
+                </form>
+                <ul> {this.state.comments.map((comment) => <li>{comment}</li>)}</ul>
+            </div>
         )
     }
 }
